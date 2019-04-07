70ae5548d5d50bbdc771a2ee80617724dac8653e
hikeislife <hikeislife@users.noreply.github.com>
2019-04-07 10:57:26 +0200

    mailer

--- mailer.php
+++ mailer.php
@@ -3,10 +3,12 @@ if (isset($_POST['submit'])) {
  $name = $_POST['name'];
  $email = $_POST['email'];
  $message = $_POST['message'];
+ 
  $subj = "Mail sa sajta";
  $mailTo = "neda@example.com";
  $headers = "From: ".$email;
  $txt = "Poruka od ".$name.".\n\n".$message;
+
  mail($mailTo, $subj, $txt, $headers);
  header("Location: index.html?poruka_poslata");
 }
